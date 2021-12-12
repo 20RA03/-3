ddce22740d8fd058eb572748d43c2cbbf6c37e8e
Alex <Alex@WIN-LQU470F1FRV>
2021-12-13 00:07:42 +0300

    Добавьте файлы проекта.

---
+++ ПР3/CALCULETE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПР3
+{
+    public static class CALCULATE
+    {
+        public static string CountOfNumbers(this Array myarray)
+        {
+
+            List<int> maxColumnValue = new List<int>();
+
+            for (int i = 0; i < myarray.RowLength; i++)
+            {
+                int tempmax = int.MinValue;
+
+                for (int j = 0; j < myarray.ColumnLength; j++)
+                {
+                    if (myarray[j, i] > tempmax)
+                    {
+                        tempmax = myarray[j, i];
+                    }
+
+                    if (j == myarray.ColumnLength - 1)
+                    {
+                        maxColumnValue.Add(tempmax);
+                    }
+                }
+            }
+            return string.Join(" ", maxColumnValue);
+        }
+    }
+}
